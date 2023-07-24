47305313886461f3d795c6b320ab23d4ab3afa38
Aguilar200 <at73934838@idat.edu.pe>
2023-07-24 14:11:38 -0500

    iniciar commit

---
+++ src/main/java/com/example/AGUILAR_ec3/AguilarEc3Application.java
@@ -0,0 +1,13 @@
+package com.example.AGUILAR_ec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguilarEc3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguilarEc3Application.class, args);
+	}
+
+}
